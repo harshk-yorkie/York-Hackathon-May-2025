c9ced0523aedd30dbf07a65a1734226910e5b662
york-team-6 <yorkhackathonteam6@gmail.com>
2025-05-31 16:13:17 +0530

    remove unused code

--- server/src/utils/routeRegister.ts
+++
@@ -1,8 +0,0 @@
-import { Express } from 'express';
-import { RouteConfig } from '../types/express';
-
-export const registerRoutes = (app: Express, routes: RouteConfig[]): void => {
-  routes.forEach(({ path, method, handler, middlewares = [] }) => {
-    app[method](path, ...middlewares, handler);
-  });
-}; 
